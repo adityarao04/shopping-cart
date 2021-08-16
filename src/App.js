848b892e59395f7eda73c607f5996c8662269e55
Aditya <adityarao1999.ar@gmail.com>
2021-08-16 20:50:33 +0530

    making Navbar component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 // import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      {/* <h1>Cart</h1> */}
      {/* <CartItem /> */}
      <Cart/>
     </div>
   );
 }
+
+
+
 
 export default App;
